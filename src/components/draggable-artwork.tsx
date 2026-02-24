20a4df56bfcdcac6806514c88bb79f8aae2b0477
Heitor Silvano <heitor.nunes182@hotmail.com>
2026-02-23 23:05:12 -0300

    chore: removing draggable from images

--- src/components/draggable-artwork.tsx
+++ src/components/draggable-artwork.tsx
@@ -5,22 +5,21 @@ import { useRef } from "react";
 const DraggableArtwork = (props: { title: string; imageFile: string }) => {
   const nodeReference = useRef<HTMLDivElement>(null);
   const startXPosition = Math.floor(Math.random() * (screen.width - 75));
-  const startYPosition = Math.floor(Math.random() * (screen.height - 150));
+  const startYPosition = Math.floor(Math.random() * (screen.height - 350));
 
   return (
     <Draggable
       nodeRef={nodeReference}
       defaultPosition={{ x: startXPosition, y: startYPosition }}
-      bounds={false}
     >
       <div
         ref={nodeReference}
-        className="w-75 h-full text-center align-middle border border-gray-300 shadow-2xl"
+        className="w-75 h-full text-center align-middle border border-gray-300 shadow-2xl cursor-grab"
       >
         <div className="w-full bg-white p-2 border-b border-gray-300">
-          {props.title}
+           {props.title}
         </div>
-        <Image src={props.imageFile} alt="artwork" className="w-full"></Image>
+        <Image src={props.imageFile} draggable={false} alt="artwork" className="w-full"></Image>
       </div>
     </Draggable>
   );
